294f62695a08a87f20255a255e0b46c95cd25d75
ttnguy19 <114105118+ttnguy19@users.noreply.github.com>
2023-03-28 17:11:57 -0400

    2nd version: constructA.m fixed
    A matrix's coefficient will now tell if the force member is in compression or tension to joints

--- EK301_Truss_Project/constructA.m
+++ EK301_Truss_Project/constructA.m
@@ -18,7 +18,7 @@ Ay = zeros(jointsNum, memberNums);
     % Find the indices of the joints connected by the member by
     % creating a vector which stores the joint number that is connected to
     % that member
-        indices = find(C(:,i))
+        indices = find(C(:,i));
         
     % Calculate the length for each member by finding the coordinate of the 2
     % joints making up the member, then using the norm function to find the
@@ -28,10 +28,12 @@ Ay = zeros(jointsNum, memberNums);
         member_length = norm(point_1 -point_2);
         
     % Calculate the coefficient and adding them to matrix Ax and Ay
-        Ax(indices(1), i) = (X(indices(1)) - X(indices(2))) /  member_length;
-        Ax(indices(2), i) = (X(indices(2)) - X(indices(1))) /  member_length;
-        Ay(indices(1), i) = (Y(indices(1)) - Y(indices(2))) /  member_length;
-        Ay(indices(2), i) = (Y(indices(2)) - Y(indices(1))) /  member_length;
+        Ax(indices(1), i) = (X(indices(2)) - X(indices(1))) /  member_length;
+        Ax(indices(2), i) = (X(indices(1)) - X(indices(2))) /  member_length;
+        Ay(indices(1), i) = (Y(indices(2)) - Y(indices(1))) /  member_length;
+        Ay(indices(2), i) = (Y(indices(1)) - Y(indices(2))) /  member_length;
+        
+        %
     end
 
     % Concatenate Ax, Ay, Sx, and Sy into a single matrix A
